7590b1084a32daaded8a8ad5f5a8f548351d1206
Mahdi AlKhalaf <mahdi@mahdialkhalaf.com>
2019-12-25 23:37:03 +0300

    Create the layout.

---
+++ js/create-cards.js
@@ -0,0 +1,19 @@
+const fragment = document.createDocumentFragment();
+
+// create four rows of cards
+for (let i = 0; i < 4; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row", "container-fluid", "align-items-center", "justify-content-center");
+
+    // create four cards in a row
+    for (let j = 0; j < 4; j++) {
+        const card = document.createElement("div");
+        card.classList.add("col-auto", "game-card");
+        row.appendChild(card);
+    }
+
+    fragment.appendChild(row);
+}
+
+const cardsContainer = document.getElementById("cards-area");
+cardsContainer.appendChild(fragment);
